b8f530ba3a26ac2a7e71ff55a91c2758a12876db
HerrCraziDev <herrcrazi@gmail.com>
2021-04-12 00:07:51 +0200

    Added validation

--- src/main/java/co/chen/servlet/CreationClient.java
+++ src/main/java/co/chen/servlet/CreationClient.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*; 
+import javax.servlet.http.*;
+import javax.swing.text.html.HTMLDocument.RunElement;
+
+import co.chen.bean.Client;
+import co.chen.form.CreationClientForm;
+import co.chen.service.ClientManager; 
 
 
 @WebServlet("/createClient")
@@ -22,58 +27,35 @@ public class CreationClient extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-        String name, surname, city, postcode, address, mail, phone, passwd, passwd_confirm;
-        Boolean dirty = false;
+        String target;
+        CreationClientForm form = new CreationClientForm();
+        Client client = new Client();
 
         request.setCharacterEncoding("UTF-8");
 
-        if ( (name = request.getParameter("name")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : name");
-        }
-        if ( (surname = request.getParameter("surname")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : surname");
-        }
-        if ( (city = request.getParameter("city")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : city");
-        }
-        if ( (postcode = request.getParameter("postcode")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : postcode");
-        }
-        if ( (address = request.getParameter("address")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : address");
-        }
-        if ( (phone = request.getParameter("phone")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : phone");
-        }
-        if ( (mail = request.getParameter("mail")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : mail");
-        }
-        if ( (passwd = request.getParameter("passwd")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : passwd");
-        }
-        if ( (passwd_confirm = request.getParameter("passwdconfirm")).isEmpty() ) {
-            dirty = true;
-            System.out.println("[Servlet] [CreationClient] POST /createClient WARN : Missing parameter : passwdconfirm");
+        // Validate form
+        client = form.validate(request);
+        request.setAttribute("form", form);
+        
+        // Routing and creation if no errors
+        if ( form.hasErrors() ) {
+            // There are errors
+            request.setAttribute("error", true);
+            target = jsp_createClient;
+        } else {
+            // There is no error, safe to continue
+            ClientManager cm = new ClientManager();
+            cm.createClient(client);
+            
+            request.setAttribute("error", false);
+            target = jsp_displayClient;
+            System.out.println("[Servlet] [CreationClient] POST /createClient INFO : All OK, forwarding");
         }
 
-        if (dirty) {
-            System.out.println("[Servlet] [CreationClient] POST /createClient --> Forwarding to " + jsp_createClient);
-            request.setAttribute("error", true);
-            this.getServletContext().getRequestDispatcher(jsp_createClient).forward(request, response);
-        } else {
-            request.setAttribute("error", false);
-            System.out.println("[Servlet] [CreationClient] POST /createClient INFO : All OK, forwarding");
-            System.out.println("[Servlet] [CreationClient] POST /createClient --> Forwarding to " + jsp_displayClient);
-            this.getServletContext().getRequestDispatcher(jsp_displayClient).forward(request, response);
-        }
+        request.setAttribute("client", client);
+
+        System.out.println("[Servlet] [CreationClient] POST /createClient --> Forwarding to " + target);
+        this.getServletContext().getRequestDispatcher(target).forward(request, response);
     }
     //
 }
